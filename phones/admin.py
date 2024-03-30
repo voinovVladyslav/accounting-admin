5fcc4c8346093b02c585976cf866764c2bf05886
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-30 12:49:24 +0200

    feat: setup phone model + admin

--- phones/admin.py
+++ phones/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import Phone
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = (
+        'imei',
+        'name',
+        'estimated_price',
+        'status',
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'name',
+        'imei',
+    )
--- phones/models.py
+++ phones/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class PhoneStatus(models.TextChoices):
+    BOUGHT = "Bought"
+    IN_TRANSIT = "In Transit"
+    IN_PROGRESS = "In Progress"
+    PENDING_SALE = "Pending Sale"
+    SOLD = "Sold"
+    ERROR = "Error"
+
+
+class Phone(models.Model):
+    name = models.CharField(
+        max_length=200,
+    )
+    imei = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name="IMEI",
+    )
+    estimated_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=PhoneStatus.choices,
+        default=PhoneStatus.BOUGHT,
+    )
