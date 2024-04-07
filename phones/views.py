375acf753f497fd6e6c9ab6347ef38dbd4b7eed5
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-04-07 08:37:59 +0300

    feat: css shortcuts for flexbox and colors

--- phones/views.py
+++ phones/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from phones.models import Phone
+
 
 class PhonesView(View):
     def get(self, request):
-        return render(request, 'phones/main.html')
+        phones = Phone.objects.all()
+        context = {'phones': phones}
+        return render(request, 'phones/main.html', context)
 
